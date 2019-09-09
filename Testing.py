de599747edc3a346f5feb9b309cd01c3802648a0
Jemuel Marcelino <53155318+jems03@users.noreply.github.com>
2019-09-09 10:48:19 +0800

    Add files via upload

---
+++ Testing.py
@@ -0,0 +1,63 @@
+from tkinter import *
+from tkinter import messagebox
+
+EmployeeInfo = []
+
+class Adds:
+    
+
+    def __init__(self, name, department, position, rate):
+        self.name = name
+        self.department = department
+        self.position = position
+        self.rate = rate
+
+def get_info(self):
+    return self.name + self.department + self.position + self.rate
+
+def thanks():
+    th = "Employee Added"
+    messagebox.showinfo("Notification", th)
+    th2 = en1.get() + d1.get() + p1.get() + er1.get()
+    messagebox.showinfo("Notification", th2)
+
+def info():
+    global EmployeeInfo
+    name = en1.get()
+    dept = d1.get()
+    pos = p1.get()
+    erate = er1.get()
+    EmployeeInfo.append(Adds(name,dept,pos,erate))
+    for a in EmployeeInfo:
+        print(a.name, a.department, a.position, a.rate)
+        
+
+def clear():
+    en1.delete(0, END)
+    d1.delete(0, END)
+    p1.delete(0, END)
+    er1.delete(0, END)
+
+fr = Tk()
+Label(fr, text='Employee Name:').grid(row=0,padx=20,pady=10)
+Label(fr, text='Department:').grid(row=1,padx=20,pady=10)
+Label(fr, text='Position:').grid(row=2,padx=20,pady=10)
+Label(fr, text='Employee Rate:').grid(row=3,padx=20,pady=10)
+Label(fr, text='Name').grid(row=5,column=0,padx=20,pady=10)
+Label(fr, text='Department').grid(row=5,column=1,padx=20,pady=10)
+Label(fr, text='Position').grid(row=5,column=2,padx=20,pady=10)
+Label(fr, text='Rate').grid(row=5,column=3,padx=20,pady=10)
+
+en1 = Entry(fr)
+d1 = Entry(fr)
+p1 = Entry(fr)
+er1 = Entry(fr)
+
+en1.grid(row=0,column=1,padx=20,pady=10)
+d1.grid(row=1,column=1,padx=20,pady=10)
+p1.grid(row=2,column=1,padx=20,pady=10)
+er1.grid(row=3,column=1,padx=20,pady=10)
+
+btn1 = Button(fr, text='Insert', command=lambda:[thanks(),info(),clear()])
+btn1.grid(row=4,column=1,padx=20,pady=10)
+fr.mainloop()
